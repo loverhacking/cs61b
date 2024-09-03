15379ff23b770423fac4f0dea12a3cf43d3bb709
Your Name <you@example.com>
2024-09-03 21:41:33 +0800

    completed Leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,15 @@ public class LeapYear {
         }
     }
 
+    /** return true if year is Leapyear 
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year){
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            return True
+        return False
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
