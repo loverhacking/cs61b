856a1acfe2e00973269410484f691f0b9e57ed27
zjy <you@example.com>
2024-11-05 00:27:08 +0800

    finish hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,104 @@
+
+package synthesizer;
+import java.util.Iterator;
+
+
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+
+        super();
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer Overflow");
+        }
+
+        rb[last] = x;
+        last = (last + 1) % capacity;
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        T x = rb[first];
+        first = (first + 1) % capacity;
+        fillCount--;
+        return x;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        return rb[first];
+    }
+
+
+    private class KeyIterator implements Iterator<T> {
+        private int count;
+        private int cur;
+
+        public KeyIterator() {
+            count = 0;
+            cur = first;
+        }
+        public boolean hasNext() {
+            return (count < capacity);
+        }
+        public T next() {
+
+            T returnItem = rb[cur];
+            cur = (cur + 1) % capacity;
+            count = count + 1;
+            return returnItem;
+        }
+    }
+
+    /** return an iterator */
+    @Override
+    public Iterator<T> iterator() {
+        return new KeyIterator();
+    }
+
+
+
+
+
+
+}
