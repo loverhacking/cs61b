8637c6e287cd4c26bd2ed7e39d8e65467ce01864
zjy <you@example.com>
2024-11-09 20:33:34 +0800

    finish proj2 phase1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -2,12 +2,21 @@ package byog.Core;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.Random;
+import java.util.Stack;
 
 public class Game {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+
+    private static Random random;
+    Stack<TETile[][]> stack = new Stack<>();
+
+
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
@@ -28,11 +37,233 @@ public class Game {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] playWithInputString(String input) {
-        // TODO: Fill out this method to run the game using the input passed in,
+
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
 
-        TETile[][] finalWorldFrame = null;
-        return finalWorldFrame;
+        TETile[][] world = null;
+        char input0 = input.charAt(0);
+        switch (Character.toUpperCase(input0)) {
+            case 'N':
+                world = newGame(input);
+                break;
+            case 'L':
+                world = loadGame();
+                break;
+            case 'Q':
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
+
+        return world;
     }
+
+    private TETile[][] loadGame() {
+        if (stack.empty()) {
+            System.exit(0);
+            return null;
+        } else {
+            return stack.pop();
+        }
+
+
+    }
+
+    private TETile[][] newGame(String input) {
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        int indexS = input.toLowerCase().indexOf('s');
+        long seed = convertSeed(input.substring(1, indexS));
+
+        initialMaze(world);
+        createMaze(world, seed);
+        savegame(world);
+        return world;
+
+    }
+
+    private void savegame(TETile[][] world) {
+        stack.push(world);
+    }
+
+    private long convertSeed(String seedString) {
+        return Long.valueOf(seedString.toString());
+    }
+
+    private static void drawWall(TETile[][] world, int x1, int x2, int y1, int y2, int x, int y) {
+
+        for (int i = x1; i <= x2; i++) {
+            world[i][y] = Tileset.WALL;
+        }
+        for (int i = y1; i <= y2; i++) {
+            world[x][i] = Tileset.WALL;
+        }
+    }
+    private static int convertBooleanToInt(boolean bool) {
+        return bool ? 1 : 0;
+    }
+    private static boolean isConnectTwoArea(TETile[][] world, int rx, int ry) {
+
+        int numIsWall = convertBooleanToInt(world[rx - 1][ry].equals(Tileset.WALL))
+                +  convertBooleanToInt(world[rx + 1][ry].equals(Tileset.WALL))
+                +  convertBooleanToInt(world[rx][ry - 1].equals(Tileset.WALL))
+                +  convertBooleanToInt(world[rx][ry + 1].equals(Tileset.WALL));
+
+        return numIsWall > 2;
+    }
+
+    private static void initialMaze(TETile[][] world) {
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+
+    private static void createMazeInOneArea(TETile[][] world, int x1, int y1, int x2, int y2, int x, int y) {
+
+        // randow pick 3 walls
+        int[] r = {0, 0, 0, 0};
+        r[Game.random.nextInt(4)] = 1;
+
+        // make holes at random points on the wall
+        for (int i = 0; i < 4; i++) {
+            if (r[i] == 0) {
+                int rx = x;
+                int ry = y;
+
+                switch (i) {
+                    case 0:
+                        do {
+                            rx = x1 + Game.random.nextInt(x - x1);
+                        } while (isConnectTwoArea(world, rx, ry));
+                        break;
+                    case 1:
+                        do {
+                            ry = y + 1 + Game.random.nextInt(y2 - y);
+                        } while (isConnectTwoArea(world, rx, ry));
+                        break;
+                    case 2:
+                        do {
+                            rx = x + 1 + Game.random.nextInt(x2 - x);
+                        } while (isConnectTwoArea(world, rx, ry));
+                        break;
+                    case 3:
+                        do {
+                            ry = y1 + Game.random.nextInt(y - y1);
+                        } while (isConnectTwoArea(world, rx, ry));
+                        break;
+                    default:
+                        break;
+                }
+                world[rx][ry] = Tileset.FLOOR;
+            }
+
+
+        }
+    }
+
+    private static void createWallBoundary(TETile[][] world, int x1, int x2, int y1, int y2) {
+        for (int x = x1 - 1; x <= x2 + 1; x += 1) {
+            world[x][y1 - 1] = Tileset.WALL;
+            world[x][y2 + 1] = Tileset.WALL;
+        }
+
+        for (int y = y1 - 1; y <= y2 + 1; y += 1) {
+            world[x1 - 1][y] = Tileset.WALL;
+            world[x2 + 1][y] = Tileset.WALL;
+        }
+    }
+
+    private static void createMaze(TETile[][] world, long seed) {
+
+        Game.random = new Random(seed);
+        int x1 = 1 + Game.random.nextInt(WIDTH - 2);
+        int x2 = x1 + 1 + Game.random.nextInt(WIDTH - 2 - x1);
+        int y1 = 1 + Game.random.nextInt(HEIGHT - 2);
+        int y2 = y1 + 1 + Game.random.nextInt(HEIGHT - 2 - y1);
+
+        createWallBoundary(world, x1, x2, y1, y2);
+
+        createMaze(world, x1, y1, x2, y2);
+        fillupMaze(world, x1, y1, x2, y2);
+        createLockedDoor(world, x1, y1, x2, y2);
+    }
+
+    private static void createLockedDoor(TETile[][] world, int x1, int y1, int x2, int y2) {
+        int flag = Game.random.nextInt(4);
+        int num;
+        switch (flag) {
+            case 0:
+                do {
+                    num = x1 + Game.random.nextInt(x2 - x1 + 1);
+                } while (world[num][y1].equals(Tileset.WALL));
+                world[num][y1 - 1] = Tileset.LOCKED_DOOR;
+                break;
+            case 1:
+                do {
+                    num = x1 + Game.random.nextInt(x2 - x1 + 1);
+                } while (world[num][y2].equals(Tileset.WALL));
+                world[num][y2 + 1] = Tileset.LOCKED_DOOR;
+                break;
+            case 2:
+                do {
+                    num = y1 + Game.random.nextInt(y2 - y1 + 1);
+                } while (world[x1][num].equals(Tileset.WALL));
+                world[x1 - 1][num] = Tileset.LOCKED_DOOR;
+                break;
+            case 3:
+                do {
+                    num = y1 + Game.random.nextInt(y2 - y1 + 1);
+                } while (world[x2][num].equals(Tileset.WALL));
+                world[x2 + 1][num] = Tileset.LOCKED_DOOR;
+                break;
+            default:
+        }
+
+    }
+
+    private static void fillupMaze(TETile[][] world, int x1, int y1, int x2, int y2) {
+        for (int x = x1; x <= x2; x += 1) {
+            for (int y = y1; y <= y2; y += 1) {
+                if (!world[x][y].equals(Tileset.WALL)) {
+                    world[x][y] = Tileset.FLOOR;
+                }
+            }
+        }
+    }
+
+    /**
+     * based on recursive partition algorithm
+     */
+    public static void createMaze(TETile[][] world, int x1, int y1, int x2, int y2) {
+
+        // base case
+        if (x2 - x1 < 2 || y2 - y1 < 2) {
+            return;
+        }
+
+        // random pick point
+        int x = x1 + 1 + Game.random.nextInt(x2 - x1 - 1);
+        int y = y1 + 1 + Game.random.nextInt(y2 - y1 - 1);
+
+        // draw wall
+        drawWall(world, x1, x2, y1, y2, x, y);
+
+        // recursive partition to continue divide maze
+        createMaze(world, x1, y1, x - 1, y - 1);
+        createMaze(world, x + 1, y + 1, x2, y2);
+        createMaze(world, x + 1, y1, x2, y - 1);
+        createMaze(world, x1, y + 1, x - 1, y2);
+
+        // create maze in one area
+        createMazeInOneArea(world, x1, y1, x2, y2, x, y);
+
+    }
+    
+
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,155 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 60;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+
+        return s + 2 * effectiveI;
+    }
+
+    /**
+     * Computesrelative x coordinate of the leftmost tile in the ith
+     * row of a hexagon, assuming that the bottom row has an x-coordinate
+     * of zero. For example, if s = 3, and i = 2, this function
+     * returns -2, because the row 2 up from the bottom starts 2 to the left
+     * of the start position, e.g.
+     *   xxxx
+     *  xxxxxx
+     * xxxxxxxx
+     * xxxxxxxx <-- i = 2, starts 2 spots to the left of the bottom of the hex
+     *  xxxxxx
+     *   xxxx
+     *
+     * @param s size of the hexagon
+     * @param i row num of the hexagon, where i = 0 is the bottom
+     * @return
+     */
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+
+    /** Adds a row of the same tile.
+     * @param world the world to draw on
+     * @param p the leftmost position of the row
+     * @param width the number of tiles wide to draw
+     * @param t the tile to draw
+     */
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        for (int xi = 0; xi < width; xi += 1) {
+            int xCoord = p.x + xi;
+            int yCoord = p.y;
+            world[xCoord][yCoord] = TETile.colorVariant(t, 32, 32, 32, RANDOM);
+
+        }
+    }
+
+    /**
+     * Adds a hexagon to the world.
+     * @param world the world to draw on
+     * @param p the bottom left coordinate of the hexagon
+     * @param s the size of the hexagon
+     * @param t the tile to draw
+     */
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+
+        if (s < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
+        }
+
+        // hexagons have 2*s rows. this code iterates up from the bottom row,
+        // which we call row 0.
+        for (int yi = 0; yi < 2 * s; yi += 1) {
+            int thisRowY = p.y + yi;
+
+            int xRowStart = p.x + hexRowOffset(s, yi);
+            Position rowStartP = new Position(xRowStart, thisRowY);
+
+            int rowWidth = hexRowWidth(s, yi);
+
+            addRow(world, rowStartP, rowWidth, t);
+
+        }
+    }
+
+    public static int multiHexagonColNumberOffset(int i) {
+        if (i <= 3) {
+            return i + 2;
+        }
+        return 5 - (i - 3);
+
+    }
+
+    public static int multiHexagonRowNumberOffset(int i, int s) {
+        if (i < 3) {
+            return -s;
+        } else if (i == 5) {
+            return 0;
+        } else {
+            return s;
+        }
+
+    }
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(2);
+        switch (tileNum) {
+            case 0: return Tileset.WALL;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.NOTHING;
+            default: return Tileset.NOTHING;
+        }
+    }
+
+    public static void main (String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        int s = 3;
+        int multiHexagonStartRowx = 2;
+
+        Position p = new Position(s, 2 * s * multiHexagonStartRowx);
+
+        int x;
+        for (x = 1; x <= 5; x += 1) {
+            int pyStart = p.y;
+            for (int y = 1; y <= multiHexagonColNumberOffset(x); y += 1) {
+                p.y += 2 * s;
+                addHexagon(world, p, s, randomTile());
+            }
+            p.x += (2 * s - 1);
+            p.y = pyStart + multiHexagonRowNumberOffset(x, s);
+        }
+
+
+        // draws the world to the screen
+        ter.renderFrame(world);
+    }
 }
